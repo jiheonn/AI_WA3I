257022ae60b03ed91b4b5e5afc32640e3df6d046
Jiheon Lee <wlgjsdl0@gmail.com>
2020-06-16 22:42:43 +0900

    modify teacher/views.py

--- wa3i/teacher/views.py
+++ wa3i/teacher/views.py
@@ -5,10 +5,6 @@ from .models import User
 from django.http import JsonResponse
 from django.db.models import Q
 
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from django.views.generic import View
-
 import datetime
 import string
 import random
